0b4c5d5c9ce0e5264e392bbd7d6f50d752086b98
Tom Grushka <tom@grushka.com>
2013-07-11 23:16:22 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lsatools::Application.config.secret_token = '6f135c5c6e38970c0a138a30c733a1055dadb8633913d427fd615df155bc03db7ee2b83d0e88d29a6f76c33c722c55536252dec844e5550a674f5b2e54b193fc'
